59de928de188a13fc0a43a5b4ad329e22e3cc7cd
gopinath <gopinathkaiser@gmail.com>
2023-11-14 14:12:36 +0530

    added conditionalOnBean also

---
+++ src/main/java/com/autoConfigEg/autoConfiguration/Model/School.java
@@ -0,0 +1,7 @@
+package com.autoConfigEg.autoConfiguration.Model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class School {
+}
--- src/main/java/com/autoConfigEg/autoConfiguration/config/StudentsConfig.java
+++ src/main/java/com/autoConfigEg/autoConfiguration/config/StudentsConfig.java
@@ -1,8 +1,10 @@
 package com.autoConfigEg.autoConfiguration.config;
 
 import com.autoConfigEg.autoConfiguration.Model.College;
+import com.autoConfigEg.autoConfiguration.Model.School;
 import com.autoConfigEg.autoConfiguration.Model.Students;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +14,8 @@ import org.springframework.context.annotation.Configuration;
 @ConditionalOnClass(value = College.class)
 public class StudentsConfig {
 
+//    @ConditionalOnBean(name = "com.autoConfigEg.autoConfiguration.Model.School")
+    @ConditionalOnBean(value = School.class)
     @Bean
     public Students students(){
         return new Students();
